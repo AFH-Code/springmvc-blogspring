f7114a8e28f7b19f54ae905e92c83273f1daab06
NZTY2298 <noel.kenfack@yahoo.fr>
2023-04-28 10:48:37 +0100

    update

--- src/main/java/com/afhunt/blogspring/entity/NewsEntity.java
+++ src/main/java/com/afhunt/blogspring/entity/NewsEntity.java
@@ -3,6 +3,9 @@ package com.afhunt.blogspring.entity;
 import jakarta.persistence.*;
 import lombok.Data;
 
+import java.util.HashSet;
+import java.util.Set;
+
 @Entity
 @Data
 public class NewsEntity {
@@ -18,6 +21,12 @@ public class NewsEntity {
     @JoinColumn(name="category")
     private CategoryEntity category;
 
+    @ManyToMany(cascade=CascadeType.PERSIST)
+    @JoinTable(name="tag_news",
+            joinColumns = { @JoinColumn(name="newsid") },
+            inverseJoinColumns = { @JoinColumn(name="tagid") })
+    private Set<TagEntity> tags = new HashSet<TagEntity>();
+
     protected NewsEntity() {
         this(null, null);
     }
@@ -26,6 +35,7 @@ public class NewsEntity {
         this.description = description;
         this.name = name;
     }
+
     public NewsEntity(String name, String description, CategoryEntity category) {
         this.id = null;
         this.description = description;
---
+++ src/main/java/com/afhunt/blogspring/entity/TagEntity.java
@@ -0,0 +1,34 @@
+package com.afhunt.blogspring.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Data
+public class TagEntity {
+    private @Id
+    @GeneratedValue
+    Long id;
+
+
+    @Column(name="name", nullable=false, length=512)
+    public String name;
+
+    @Column(name="description", nullable=false, length=512)
+    public String description;
+
+    @ManyToMany(mappedBy="tags")
+    private Set<NewsEntity> news = new HashSet<NewsEntity>();
+
+    protected TagEntity() {
+        this(null, null);
+    }
+    public TagEntity(String name, String description) {
+        this.id = null;
+        this.description = description;
+        this.name = name;
+    }
+}
